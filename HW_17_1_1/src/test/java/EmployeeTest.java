f7680f0cc109d53906d7223a7091cc0ed6452585
Alena Kuts <ruale@inbox.ru>
2021-10-31 15:16:44 +0300

    add HW_17_1_1

---
+++ HW_17_1_1/src/test/java/EmployeeTest.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EmployeeTest {
+
+    Employee employee = new Employee("Anna", 35, 'F', 50);
+
+    @Test
+    void testGetSalary(){
+        int expectedSalary = 3300;
+        int actualSalary = employee.getSalary(MonthUtils.MONTHS_QUARTER1);
+
+        Assert.assertEquals(actualSalary, expectedSalary);
+    }
+
+    @Test
+    void testGetName(){
+        String expectedName = "Anna";
+        String actualName = employee.getName();
+
+        Assert.assertEquals(actualName, expectedName);
+    }
+
+    @Test
+    void testGetAge(){
+        int expectedAge = 35;
+        int actualAge = employee.getAge();
+
+        Assert.assertEquals(actualAge, expectedAge);
+    }
+
+    @Test
+    void testGetGender(){
+        char expectedGender = 'F';
+        char actualGender = employee.getGender();
+
+        Assert.assertEquals(actualGender, expectedGender);
+    }
+
+}
